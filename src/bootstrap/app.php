2821f41f2c49d399c0b5cf09ec84180349aec20d
Volodymyr <ymyr.vlad@gmail.com>
2025-03-16 16:36:37 +0200

    [MOV-27] add force json response for API

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -12,7 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->group('api', [
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // 'throttle:api',
+           \App\Http\Middleware\ForceJsonResponse::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
