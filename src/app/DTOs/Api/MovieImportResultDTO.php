119666bed67aa74a728f7f92b71cd861c214589d
Volodymyr <ymyr.vlad@gmail.com>
2025-03-30 15:52:55 +0300

    [MOV-28] add api for PUT movie info

---
+++ src/app/DTOs/Api/MovieImportResultDTO.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DTOs\Api;
+
+class MovieImportResultDTO
+{
+    public array $queued;
+    public array $failed;
+
+    public function __construct(array $queued, array $failed)
+    {
+        $this->queued = $queued;
+        $this->failed = $failed;
+    }
+    public function toArray(): array
+    {
+        return [
+            'queued' => $this->queued,
+            'failed' => $this->failed,
+        ];
+    }
+}
