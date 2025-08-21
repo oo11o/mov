683aed50a3ff3e798d0ce8764a4e11299da5b5b9
Volodymyr <ymyr.vlad@gmail.com>
2025-08-21 06:46:31 +0300

    [MOV-30] test: expect controller to return SimilarMoviesResponseDto with metadata and movies

--- src/tests/Feature/SimilarMoviesControllerTest.php
+++ src/tests/Feature/SimilarMoviesControllerTest.php
@@ -18,14 +18,25 @@ class SimilarMoviesControllerTest extends TestCase
             $mock->shouldReceive('getSimilarMovies')
                 ->with('Terminator')
                 ->andReturn(
-                    collect([
-                        new MovieDto('Robocop', 2014),
-                        new MovieDto('Terminator 2', 2006),
-                    ])
+                    new SimilarMovieArtircleDTO(
+                        title: 'Similar Movies: Terminator',
+                        description: 'Description of Terminator movies',
+                        h1: 'Top Similar Movies',
+                        intro: 'Here are some movies similar to Terminator',
+                        movies: collect([
+                            new MovieDto('Robocop', 2014),
+                            new MovieDto('Terminator 2', 2006),
+                        ])
+                    )
                 );
         });
 
         $response = $this->get('/similar/Terminator');
+
+        $response->assertSee('Similar Movies: Terminator');
+        $response->assertSee('Description of Terminator movies');
+        $response->assertSee('Top Similar Movies');
+        $response->assertSee('Here are some movies similar to Terminator');
 
         $response->assertStatus(200);
         $response->assertSee('Robocop');
